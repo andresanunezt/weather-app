4fbe04134595f7e74f36b1dc939efd784032b697
Andrés Nuñez <andresanunez98@gmail.com>
2021-08-09 16:10:24 -0400

    created component folder/added weather.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-
-function App() {
+// import Weather from './components/weather';
+export default function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,4 +28,36 @@ function App() {
   return <div className="App"></div>;
 }
 
-export default App;
+// import "./App.css";
+// import React, { useEffect, useState } from "react";
+
+// function App() {
+//   const [lat, setLat] = useState([]);
+//   const [long, setLong] = useState([]);
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       navigator.geolocation.getCurrentPosition(function (position) {
+//         setLat(position.coords.latitude);
+//         setLong(position.coords.longitude);
+//       });
+
+//       await fetch(
+//         `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+//       )
+//         .then((res) => res.json())
+//         .then((result) => {
+//           debugger;
+//           console.log(result);
+//           setData(result);
+//           console.log(result);
+//         });
+//     };
+//     fetchData();
+//   }, [lat, long]);
+
+//   return <div className="App"></div>;
+// }
+
+// export default App;
