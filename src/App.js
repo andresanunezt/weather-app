801580659975b0fad575503b0b089452b774e619
Andrés Nuñez <andresanunez98@gmail.com>
2021-08-09 15:31:30 -0400

    began implementing fetch from API

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ function App() {
   const [long, setLong] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
 
-    console.log("Latitude is:", lat);
-    console.log("Longitude is:", long);
+      await fetch(
+        `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
   }, [lat, long]);
 
   return <div className="App"></div>;
