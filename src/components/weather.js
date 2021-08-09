0d70572e6a3a6bcf706738cff0d35d5c375a7d08
Andrés Nuñez <andresanunez98@gmail.com>
2021-08-09 19:33:29 -0400

    began implementing stylinh

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./styles.css";
+import "../style.css";
 import moment from "moment";
 import { Card } from "semantic-ui-react";
 
@@ -7,22 +7,20 @@ const CardExampleCard = ({ weatherData }) => (
   <Card>
     <Card.Content>
       <Card.Header className="header">{weatherData.name}</Card.Header>
-      <p>Temprature: {weatherData.main.temp}</p>
-      <p>Sunrise: {weatherData.sys.sunrise}</p>
-      <p>Sunset: {weatherData.sys.sunset}</p>
-      <p>Description: {weatherData.weather[0].description}</p>
+      <p className="day">
+        Day: {moment().format("dddd")}, {moment().format("LL")}{" "}
+      </p>
+      <p className="temp">Temprature: {weatherData.main.temp}</p>
       <p>
-        Sunrise formatted TIME:{" "}
+        Sunrise:{" "}
         {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString("en-US")}
       </p>
       <p>
-        Sunset formatted TIME:{" "}
+        Sunset:{" "}
         {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString("en-US")}
       </p>
+      <p>Description: {weatherData.weather[0].description}</p>
       <p>Humidity: {weatherData.main.humidity} %</p>
-      <p>
-        Day: {moment().format("dddd")}, {moment().format("LL")}{" "}
-      </p>
     </Card.Content>
   </Card>
 );
